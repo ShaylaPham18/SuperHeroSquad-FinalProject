9b746c9a5001917894ebd9fc588dc1e1d421b4e6
Razan <rabdalla@ggc.edu>
2025-04-15 14:03:42 -0400

    Razan/fix Path

--- FinalGame_Project/src/GameMain.java
+++ FinalGame_Project/src/GameMain.java
@@ -1,24 +1,20 @@
 import controller.PuzzleController;
 import loader.FileLoader;
-import model.Floors;
 import model.Puzzle;
-import model.Room;
 import view.PuzzleView;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class GameMain {
     public static void main(String[] args) {
         try {
-            // Load puzzles from file
-            List<Puzzle> puzzles = FileLoader.loadPuzzles("src/puzzles.txt");
+            // Load rooms
+            FileLoader fileLoader = new FileLoader();
+            fileLoader.readRooms();  // Reads from "room.txt"
+
+            // Load puzzles
+            List<Puzzle> puzzles = FileLoader.loadPuzzles("puzzles.txt");
             System.out.println("🧩 Loaded " + puzzles.size() + " puzzles!");
 
             // List puzzle names
@@ -47,16 +43,13 @@ public class GameMain {
 
         } catch (Exception e) {
             System.out.println("❗ Error: " + e.getMessage());
+            e.printStackTrace();
         }
-
-       GameMain gameMain=new GameMain();
-
-        FileLoader fileLoader=new FileLoader();
-        fileLoader.readRooms();
     }
 
-    private static void helpMenu(){
-        Frame frame=new Frame();
+    // Optional help menu method
+    private static void helpMenu() {
+        Frame frame = new Frame();
         frame.helpMenu();
     }
 }
