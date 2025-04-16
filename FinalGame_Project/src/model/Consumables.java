0b0a2dc85602b8e62bbd3e1aced24f396e286b3d
ShaylaPham18 <spham5@ggc.edu>
2025-04-15 22:33:01 -0400

    Added an items.txt, started wworking on items class and consumables child class

--- FinalGame_Project/src/model/Consumables.java
+++ FinalGame_Project/src/model/Consumables.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Consumables {
+//Shayla
+public class Consumables extends Items{
+    public int healPercentage;
+
+
+    //Shayla
+    /**
+     * Constructor
+     *
+     * @param id
+     * @param name
+     * @param description
+     */
+    public Consumables(int id, String name, int healPercentage, String description) {
+        super(id, name, description);
+        this.healPercentage = healPercentage;
+    }
 }
--- FinalGame_Project/src/model/Items.java
+++ FinalGame_Project/src/model/Items.java
@@ -1,4 +1,41 @@
 package model;
 
+//Shayla
 public class Items {
+    private int id;
+    private String name;
+    private String description;
+
+    //Shayla
+    /**
+     * Constructor
+     *
+     * @param id of item
+     * @param name of the item
+     * @param description of the item
+     */
+    public Items(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    //Shayla
+    /**
+     * Getter Methods:
+     * getName - Used to retrieve the item's name
+     * getDescription - Used to retrieve the item's description
+     *
+     * @return item name & description
+     */
+
+    //Shayla
+    public String getName() {
+        return name;
+    }
+
+    //Shayla
+    public String getDescription() {
+        return description;
+    }
 }
