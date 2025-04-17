6249653c0d6f445bacc7e40980c37cd38ab1c899
Razan <rabdalla@ggc.edu>
2025-04-16 21:34:55 -0400

    Razan

--- FinalGame_Project/src/loader/FileLoader.java
+++ FinalGame_Project/src/loader/FileLoader.java
@@ -48,29 +48,8 @@ public class FileLoader {
         try {
             //you guys use this 1 PLEASE JUST COMMENT AND UNCOMMENT
 
-
-
-           // BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-
-
-
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-
-           // BufferedReader bufferedReader = new BufferedReader(new FileReader("room.txt"));
             //BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-           // BufferedReader bufferedReader = new BufferedReader(new FileReader("room.txt"));
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-
-
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("room.txt"));
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("room.txt"));
-          //  BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("FinalGame_Project/room.txt"));
-            //BufferedReader bufferedReader = new BufferedReader(new FileReader("room.txt"));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("room.txt"));
 
 
 
