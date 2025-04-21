6ccc4e77d173c39a04a670cb70142e0cd0caea8f
Razan <rabdalla@ggc.edu>
2025-04-21 16:45:10 -0400

    Razan -->fix Puzzle

--- FinalGame_Project/src/view/Frame.java
+++ FinalGame_Project/src/view/Frame.java
@@ -58,8 +58,8 @@ public class Frame {
 
         public void map(){//justin
                 JFrame frame = new JFrame("Map");
-                ImageIcon imageIcon = new ImageIcon("FinalGame_Project/HospitalMap.png");
                 //ImageIcon imageIcon = new ImageIcon("HospitalMap.png");
+                ImageIcon imageIcon = new ImageIcon("FinalGame_Project/HospitalMap.png");
                 JLabel imageLabel = new JLabel(imageIcon);
                 Image image = imageIcon.getImage().getScaledInstance(800, 600, Image.SCALE_SMOOTH);
                 imageLabel.setIcon(new ImageIcon(image));
